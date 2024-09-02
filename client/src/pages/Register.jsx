8d5d5ff3b8f74b2af8b6cc14a936d6b1a1f305c4
hwrdyen <hwrdpvt@gmail.com>
2024-09-02 13:38:04 +0800

    Drafted register form

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,20 @@
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Gabble</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="file" />
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
